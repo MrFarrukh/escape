94405194a2baf9ec89f9cdbcdba8648d04d8d312
Mr Farrukh <77459341+MrFarrukh@users.noreply.github.com>
2022-02-21 08:55:22 +0300

    Delete escape directory

--- escape/js/index.js
+++
@@ -1,6 +0,0 @@
-let burgerMenu = document.getElementById('burger-menu')
-let burgerBtn = document.getElementById('burger-btn')
-
-burgerBtn.addEventListener('click',function(){
-    burgerMenu.classList.toggle('active')
-})
